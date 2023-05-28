599c1573fcbf874a580a1c971c2d057be560fcf7
Raj praneeth dayakar <52042862+smartnikil@users.noreply.github.com>
2023-05-27 22:53:02 -0500

    Add files via upload

---
+++ Factorial/Factorial.java
@@ -0,0 +1,17 @@
+package Factorial;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int n = sc.nextInt();
+        int fact = 1;
+        for(int i =1;i<=n;i++) {
+            fact = fact * i;
+           // System.out.println("Factorial of "+ n +" is "+ fact);
+        }
+        System.out.println("Factorial of "+ n +" is "+ fact);
+    }
+}
